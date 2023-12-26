cfe487f4acc35fe8489cc8ef935dfe4096a1662e
mishraaakash <122592055+mishraaakash@users.noreply.github.com>
2023-12-26 12:01:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var http = require("http");
-http
-  .createServer(function (req, res) {
-    res.writeHead()
-    res.write("<h1>RAIT</h1>");
-    res.end("<h2>welcome to rait...</h2>");
-  })
-  .listen(8787);
+var data = require('./test')
+var url = require('url');
+
+http.createServer(function (req, res) {
+    // res.writeHead(200,{'Content-Type': 'text/html'})
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write("<h1>RAIT</h1>" + data.testModule())
+    res.write("<br>")
+    var query = url.parse(req.url,true).query;
+    console.log(query)
+    res.end("<h3>welcome to rait...</h3>")
+  }).listen(8787);
+
+
---
+++ test.js
@@ -0,0 +1,7 @@
+// exports.testModule = function(){
+//     return "this is the test module";
+// }
+
+exports.testModule = () =>{
+    return "this is a testmodule";
+}
